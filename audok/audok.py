f9f2321ed31f389f68d8a93d67d28f959d1c7ce5
japplo <>
2021-08-26 21:53:06 +0200

    show xdg folder in debug mode , release version 0.8.0

--- audok/audok.py
+++ audok/audok.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
    if sys.stdin.isatty():
       settings['Debug'] = 1
 
-   settings['Version'] = '0.7.9'
+   settings['Version'] = '0.8.0'
    # generate a new settings.xml
    settings['Min_Version'] = '0.7.5'
 
@@ -110,6 +110,7 @@ if __name__ == '__main__':
    
    if settings['Debug']==1:
       print ('- main version: %s Share_Path: %s pid: %s cwd: %s' % (settings['Version'],settings['App_Path'],settings['Pid'],os.getcwd()))
+      print ('- music path: %s config path: %s' % (settings['Music_Path'],settings['Config_Path']))
 
 
 
