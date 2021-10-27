2b3a8e44128a72d826d96cfc4e2161ae67f5418b
japplo <>
2021-10-27 21:57:29 +0200

    release version 0.8.8

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Audok is a simple linux music player with streamripper and converter support. Au
 
 Version
 ======
-Current Version is 0.8.7
+Current Version is 0.8.8
 
 
 Requirements
--- audok/audok.py
+++ audok/audok.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
       config['debug']=1
 
    config['name'] = 'audok'
-   config['version'] = '0.8.7'
+   config['version'] = '0.8.8'
 
    config['app_path'] = app_path
 
