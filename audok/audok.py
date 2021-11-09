4566fb9db1150cfdc571267afefeae01bbcd8d06
japplo <>
2021-11-09 19:33:22 +0100

    release version 0.9.2

--- audok/audok.py
+++ audok/audok.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
       config['debug']=1
 
    config['name'] = 'audok'
-   config['version'] = '0.9.1'
+   config['version'] = '0.9.2'
 
    config['app_path'] = app_path
 
