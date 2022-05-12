3dbb23a7bf54560c6d95a852c6a8c76ca22031e5
kalibari <kalibari@zoho.com>
2022-05-12 08:41:14 +0200

    updated about text

--- audok/tab_about.py
+++ audok/tab_about.py
@@ -15,18 +15,34 @@ class TabAbout:
 
       self.log.debug('start')
 
-      self.box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=0)
+      self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
 
-      label1 = Gtk.Label(label='Name: %s\nVersion: %s\nLicence: GNU GENERAL PUBLIC LICENSE Version 3\nCopyright: Copyright (c) 2020 - 2021, kalibari\nWebSite: https://github.com/kalibari/audok' % (self.config['name'].title(),self.config['version']))
-      label1.set_margin_start(5)
-      label1.set_xalign(0)
+      label1 = Gtk.Label(label='Name: %s' % self.config['name'].title())
+      label1.set_valign(Gtk.Align.CENTER)
+      label1.set_margin_top(5)
 
-      image3 = Gtk.Image()
-      image3.set_from_file('%s/audok_large.png' % self.config['app_path'])
-      image3.set_pixel_size(300)
+      label2 = Gtk.Label(label='Version: %s' % self.config['version'])
+      label2.set_valign(Gtk.Align.CENTER)
 
-      image3.props.valign = Gtk.Align.CENTER
+      image1 = Gtk.Image()
+      image1.set_from_file('%s/audok_large.png' % self.config['app_path'])
+      image1.set_pixel_size(300)
+      image1.props.valign = Gtk.Align.CENTER
+
+
+      label3 = Gtk.Label(label='Licence: GNU GENERAL PUBLIC LICENSE Version 3')
+      label3.set_valign(Gtk.Align.CENTER)
+
+      label4 = Gtk.Label(label='Copyright: Copyright (c) 2020 - 2021, kalibari')
+      label4.set_valign(Gtk.Align.CENTER)
+
+      label5 = Gtk.Label(label='WebSite: https://github.com/kalibari/audok')
+      label5.set_valign(Gtk.Align.CENTER)
+      label5.set_margin_bottom(5)
 
       self.box.append(label1)
-      self.box.append(image3)
-
+      self.box.append(label2)
+      self.box.append(image1)
+      self.box.append(label3)
+      self.box.append(label4)
+      self.box.append(label5)
